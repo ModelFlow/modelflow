d55d4c64d56a7115077527b7e396369a44eb2b3b
Kylie Holland <kylholl@stanford.edu>
2020-11-19 12:12:24 -0800

    Added mass for nuclear reactor

--- examples/mars/models/nuclear_reactor.py
+++ examples/mars/models/nuclear_reactor.py
@@ -13,9 +13,9 @@ class NuclearReactor:
             dict(
                 key="mass",
                 units="kg",
-                value=1500,
-                source="https://en.wikipedia.org/wiki/Kilopower",
-                notes="The space rated 10 kWe Kilopower for Mars is expected to mass 1500 kg in total (with a 226 kg core) and contain 43.7 kg of U235"
+                value=4500,
+                source="https://ats-fns.fi/images/files/2019/syp2019/presentations/PS2_PVenneri_NuclearTechnologyForSpaceSettlementsAndExploration.pdf",
+                notes="Mass for UltraSafe Nuclear Technology's space reactor"
             )
         ],
         "states": [],
