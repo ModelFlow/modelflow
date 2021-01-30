092274f167028722ec1b8b4176de57a2e5447ecb
Kylie Holland <kylieholland@ITs-MacBook-Pro.lan>
2021-01-30 12:24:23 -0800

    Nuclear reactor stats added

--- examples/mars/models/nuclear_reactor.py
+++ examples/mars/models/nuclear_reactor.py
@@ -6,17 +6,32 @@ class NuclearReactor:
         "params": [
             dict(
                 key="max_kw_ac",
-                units="kw",
-                value=40,
-                source="FAKE"
+                units="kwe",
+                value=150,
+                source="USNC-Tech"
             ),
             dict(
                 key="mass",
                 units="kg",
-                value=1500,
-                source="https://en.wikipedia.org/wiki/Kilopower",
-                notes="The space rated 10 kWe Kilopower for Mars is expected to mass 1500 kg in total (with a 226 kg core) and contain 43.7 kg of U235"
+                value=4500,
+                source="USNC-Tech",
+               
+            ),
+            dict(
+                key="lifespan",
+                units="years",
+                value=10,
+                source="USNC-Tech",
+               
+            ),
+            dict(
+                key="heat production",
+                units="kelvin",
+                value=1150,
+                source="USNC-Tech",
+               
             )
+
         ],
         "states": [],
         "linked_output_states": [
